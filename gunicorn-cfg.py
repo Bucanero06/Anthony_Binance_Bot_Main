9a7c200b26ff68616b0c657f2c0a33a10da34519
ruben <ruben@moonshot.codes>
2023-02-10 18:27:21 -0500

    -

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -1,9 +1,10 @@
 # -*- encoding: utf-8 -*-
 
-bind = '0.0.0.0:5005'
+bind = '0.0.0.0:8000'
 workers = 1
 # threads = 2
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
 enable_stdio_inheritance = True
+
